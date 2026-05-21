88231ca1db351a270503b0340bbd1d3754649bfa
Anthony Avila <50991131+jordanavila1394@users.noreply.github.com>
2026-05-21 11:17:09 +0200

    feat(nav): add donations link to desktop and mobile menus

--- wp-content/themes/wecoop/front-page.php
+++ wp-content/themes/wecoop/front-page.php
@@ -34,6 +34,7 @@ $lang_default = static function($it, $en, $es) use ($current_lang) {
                 <a href="<?php echo esc_url($job_offers_url); ?>"><?php echo esc_html($tr('frontpage.nav.jobs', $lang_default('Offerte di lavoro', 'Job Offers', 'Ofertas de trabajo'))); ?></a>
                 <a href="#passaparola"><?php echo esc_html($tr('frontpage.nav.passaparola', $lang_default('Passaparola', 'Passaparola', 'Passaparola'))); ?></a>
                 <a href="#plataforma"><?php echo esc_html($tr('frontpage.nav.platform', $lang_default('Piattaforma Digitale', 'Digital Platform', 'Plataforma Digital'))); ?></a>
+                <a href="<?php echo esc_url(home_url('/donazioni/')); ?>"><?php echo esc_html($tr('frontpage.nav.donations', $lang_default('Donazioni', 'Donations', 'Donaciones'))); ?></a>
                 <a href="#impacto"><?php echo esc_html($tr('frontpage.nav.impact', $lang_default('Impatto', 'Impact', 'Impacto'))); ?></a>
                 <a href="#contacto"><?php echo esc_html($tr('frontpage.nav.contact', $lang_default('Contatti', 'Contact', 'Contacto'))); ?></a>
             </div>
--- wp-content/themes/wecoop/header.php
+++ wp-content/themes/wecoop/header.php
@@ -29,6 +29,7 @@ $_tr = 'translate_string';
                 <a href="<?php echo esc_url(home_url('/annunci-lavoro-wecoop/')); ?>"><?php echo esc_html($_tr('frontpage.nav.jobs', 'Offerte di lavoro')); ?></a>
                 <a href="<?php echo esc_url(home_url('/#passaparola')); ?>"><?php echo esc_html($_tr('frontpage.nav.passaparola', 'Passaparola')); ?></a>
                 <a href="<?php echo esc_url(home_url('/#plataforma')); ?>"><?php echo esc_html($_tr('frontpage.nav.platform', 'Piattaforma Digitale')); ?></a>
+                <a href="<?php echo esc_url(home_url('/donazioni/')); ?>"><?php echo esc_html($_tr('frontpage.nav.donations', 'Donazioni')); ?></a>
                 <a href="<?php echo esc_url(home_url('/#impacto')); ?>"><?php echo esc_html($_tr('frontpage.nav.impact', 'Impatto')); ?></a>
                 <a href="<?php echo esc_url(home_url('/#contacto')); ?>"><?php echo esc_html($_tr('frontpage.nav.contact', 'Contatti')); ?></a>
             </div>
